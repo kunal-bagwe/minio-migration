ce84ca4b3344d3d7883d37c401c7d0a535325e96
kunal-bagwe <bagwe.kunal@gmail.com>
2021-07-14 19:36:25 +0530

    Update mingration-script-updated.sh

--- mingration-script-updated.sh
+++ mingration-script-updated.sh
@@ -1,49 +1,50 @@
 #!/bin/bash
-bucket='sample'
-date=`date -R`
-s3_key='AKIAIOSFODNN7EXAMPLE'
-s3_secret='wJalrXUtnFEMIK7MDENGbPxRfiCYEXAMPLEKEY'
-#content_type="application/octet-stream"
-#resource="/${bucket}/${file}"
-#_signature="PUT\n\n${content_type}\n${date}\n${resource}"
-#signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
 
-# curl -v -X PUT -T demo.txt -H "Host: 10.93.2.5:9000" -H "Date: ${date}" -H "Content-Type: ${content_type}" -H "Authorization:AWS ${s3_key}:${signature}" http://10.93.2.5:9000$resource
+# echo "source populate-kubeconfig.sh"
+# source populate-kubeconfig.sh
 
-echo "source populate-kubeconfig.sh"
-source populate-kubeconfig.sh
+# echo "Getting contexts list"
+# contexts=$(kubectl config get-contexts --no-headers -o name)
 
-echo "Getting contexts list"
-contexts=$(kubectl config get-contexts --no-headers -o name)
+# emc charts for deletion
+charts=("emc-aggregation", "emc-alarms", "emc-appmaster", "emc-correlation", "emc-edr-processing", "emc-input-adapter", "emc-output", "emc-config-node")
 
+# for context in $contexts
+# do
+#  echo ""
+#  echo "current context: $context"
 
+  echo "Configuring alias for minio cluster"
+  mc alias set minio ${MINIO_SERVICE} ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
+  if [ $? -ne 0 ]
+  then
+    echo "Unable to configure minio server, please check logs."
+    exit 1
+  fi
+  echo "Creating ${MINIO_BUCKET_NAME} bucket"
+  echo "Executing command mc mb minio/${MINIO_BUCKET_NAME}"
+  mc mb minio/${MINIO_BUCKET_NAME} > /dev/null 2>&1
+  if [ $? -ne 0 ]
+  then
+    echo "${MINIO_BUCKET_NAME} bucket already exists"
+  else
+    echo "${MINIO_BUCKET_NAME} bucket is created"
+  fi
 
-host=10.93.2.5:9000
-# access_key=$2
-# secret_key=$3
-bucket_name=sample
-
-echo "Configuring alias for minio cluster"
-mc alias set myminio http://${host} AKIAIOSFODNN7EXAMPLE wJalrXUtnFEMIK7MDENGbPxRfiCYEXAMPLEKEY
-echo "Copying the objects to bucket"
-#if [[ -d "demo" ]]
-#then
-#  ./mc cp --recursive demo/ myminio/${bucket_name}/demo/
-#else
-#  ./mc cp --recursive demo myminio/${bucket_name}/
-#fi
-
-dir=$(find . -type d -name "*")
-echo $dir
-for d in $dir
-do
-  dir_name=$(echo $d | sed "s/^./${PWD##*/}/g")
-  echo $dir_name
-  echo "Copying to minio bucket"
-  mc cp --recursive ${d} myminio/${bucket_name}/
-  file_name=$(find $d -type f -name "*")
-#  for f in $file_name
-#  do
-#  echo ${f:2}
-#  done
-done
+  echo "Executing command mc mirror --overwrite /tmp/  minio/${MINIO_BUCKET_NAME}"
+  echo "Copying data to ${MINIO_BUCKET_NAME} bucket"
+  mc mirror --overwrite /tmp/  minio/${MINIO_BUCKET_NAME}
+  echo "Copy operation completed, list ${MINIO_BUCKET_NAME} bucket for data"
+# for i in “${charts[@]}”
+# do
+#   echo "searching for ${i} chart in $context context..."
+#   HELM_KUBECONTEXT=$context helm status ${i} -n empirix-cloud &>/dev/null
+#   if [ $HELM_KUBECONTEXT -eq 0 ]; then
+#     echo "deleting ${i} chart from $context context"
+#     $context helm uninstall ${i} -n empirix-cloud
+#   else
+#     continue
+#   fi
+# done
+# done
+echo "Finished execution"
